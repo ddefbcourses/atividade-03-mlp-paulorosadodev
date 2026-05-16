8da2a9e7fd55251ae321c8fcb8b2b8f1b0142fe7
Paulo Rosado <paulorosadodev@gmail.com>
2026-05-16 01:49:21 -0300

    Entrega da Atividade Prática 3 - MLP

--- src/plots.py
+++ src/plots.py
@@ -35,7 +35,7 @@ def compare_models(names, scores, metric_name):
 
     plt.figure(figsize=(8, 4))
 
-    plt.bar(names, scores)
+    bars = plt.bar(names, scores)
 
     plt.title(
         f"Comparison of {metric_name}"
@@ -43,7 +43,21 @@ def compare_models(names, scores, metric_name):
 
     plt.ylabel(metric_name)
 
+    min_score = min(scores)
+    max_score = max(scores)
+    
+    if "Tempo" not in metric_name:
+        if max_score - min_score < 0.1:
+            plt.ylim(max(0, min_score - 0.01), min(1.05, max_score + 0.01))
+        else:
+            plt.ylim(0, 1.05)
+    else:
+        plt.ylim(0, max_score * 1.1)
+
+    for bar in bars:
+        yval = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width()/2, yval + (0.002 if "Tempo" not in metric_name else max_score * 0.02), f'{yval:.0f}' if "Tempo" in metric_name else f'{yval:.4f}', ha='center', va='bottom')
+
     plt.xticks(rotation=15)
 
     plt.show()
-```
